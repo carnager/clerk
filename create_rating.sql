7576adb3fe591bd10bc2c366874882df0a573ecb
Rasmus Steinke <rasi@xssn.at>
2014-08-26 19:34:11 +0200

    removed uris from ratings

--- create_rating.sql
+++ create_rating.sql
@@ -2,18 +2,18 @@ create table if not exists tracks(
     date varchar(12) not null,
     artist varchar(255) not null,
     album varchar(255) not null,
-    directory varchar(500) not null,
+    title varchar(255) not null,
+    trackbumber smallint not null,
     rating smallint not null
 );
 create index if not exists track_ratings_idx ON tracks (rating);
-CREATE UNIQUE INDEX tracks_dir_idx ON tracks (directory);
+CREATE UNIQUE INDEX tracks_title_idx ON tracks (title);
 
 create table if not exists albums(
     date varchar(12) not null,
     artist varchar(255) not null,
     album varchar(255) not null,
-    directory varchar(500) not null,
     rating smallint not null
 );
 create index if not exists album_ratings_idx ON albums (rating);
-CREATE UNIQUE INDEX albums_dir_idx ON albums (directory);
+CREATE UNIQUE INDEX albums_dir_idx ON albums (album);
