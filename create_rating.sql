0ef401e3922946c241aa59b893ff2ae5c48e907a
Rasmus Steinke <rasi@xssn.at>
2014-10-04 20:00:18 +0200

    finished up track ratings support, removed some options

--- create_rating.sql
+++ create_rating.sql
@@ -1,14 +1,3 @@
-create table if not exists tracks(
-    date varchar(12) not null,
-    artist varchar(255) not null,
-    album varchar(255) not null,
-    title varchar(255) not null,
-    tracknumber smallint not null,
-    rating smallint not null
-);
-create index if not exists track_ratings_idx ON tracks (rating);
-CREATE UNIQUE INDEX tracks_title_idx ON tracks (title);
-
 create table if not exists albums(
     date varchar(12) not null,
     artist varchar(255) not null,
