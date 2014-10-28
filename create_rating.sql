480ae4a52005bbcdbead30d62f8a1adf154c5cf1
Rasmus Steinke <rasi@xssn.at>
2014-10-28 20:01:42 +0100

    simplify add random function

--- create_rating.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists albums(
-    date varchar(12) not null,
-    artist varchar(255) not null,
-    album varchar(255) not null,
-    rating smallint not null
-);
-create index if not exists album_ratings_idx ON albums (rating);
-CREATE UNIQUE INDEX albums_dir_idx ON albums (album);
