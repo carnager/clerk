79928f0f3412d21c3a3bb4132b0720584a0509fb
Rasmus Steinke <rasi@xssn.at>
2017-10-20 21:21:18 +0200

    remove conflicting license

--- clerk.pl
+++ clerk.pl
@@ -657,19 +657,3 @@ clerk [command] [-f]
 clerk version 4.0
 
 =cut
-
-=head1 LICENSE
-Copyright (C) 2015-2017  Rasmus Steinke <rasi@xssn.at>
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-See LICENSE for the full license text.
-=cut
-
